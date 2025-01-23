c777fb70df577d2e4b0561720889f525314e75c1
Vrishank B <vrish.2013@gmail.com>
2025-01-23 18:51:57 -0500

    basic bitboard setup + pawn attacks

---
+++ bitboards.cpp
@@ -0,0 +1,73 @@
+#include "bitboards.h"
+
+using namespace std;
+
+Bitboard::Bitboard() {
+   bitboards[WHITE_PAWN] = 0xFF00ULL;
+   bitboards[WHITE_KNIGHT] = 0x42ULL;
+   bitboards[WHITE_BISHOP] = 0x24ULL;
+   bitboards[WHITE_ROOK] = 0x81ULL;
+   bitboards[WHITE_QUEEN] = 0x10ULL;
+   bitboards[WHITE_KING] = 0x8ULL;
+
+   bitboards[BLACK_PAWN] = 0xFF000000000000ULL;
+   bitboards[BLACK_KNIGHT] = 0x4200000000000000ULL;
+   bitboards[BLACK_BISHOP] = 0x2400000000000000ULL;
+   bitboards[BLACK_ROOK] = 0x8100000000000000ULL;
+   bitboards[BLACK_QUEEN] = 0x1000000000000000ULL;
+   bitboards[BLACK_KING] = 0x0800000000000000ULL;
+
+   filled_squares = 0ULL;
+   for (int i = 0; i < 12; i++) {
+      filled_squares |= bitboards[i];
+   }
+   empty_squares = ~filled_squares;
+
+   for (int i = 0; i < 64; i++) {
+      pawn_moves[WHITE][i] = init_pawn_moves(i, WHITE);
+      pawn_moves[BLACK][i] = init_pawn_moves(i, BLACK);
+   }
+}
+
+uint64_t init_pawn_moves(int square, int side) {
+
+   uint64_t bitboard = 0ULL;
+   uint64_t attack = 0ULL;
+
+   SET_BIT(bitboard, square);
+
+   // white
+   if (!side) {
+      attack |= (bitboard >> 7) & not_a_file;
+      attack |= (bitboard >> 9) & not_h_file;
+   }
+
+   else {
+      attack |= (bitboard << 7) & not_h_file;
+      attack |= (bitboard << 9) & not_a_file;
+   }
+
+   // black
+
+   return attack;
+}
+
+uint64_t init_horse_moves(int square);
+void print_bitboard(uint64_t bitboard) {
+
+   for (int rank = 0; rank < 8; rank++) {
+
+      for (int file = 0; file < 8; file++) {
+         if (!file) {
+            cout << (8 - rank) << "  ";
+         }
+
+         int square = rank * 8 + file;
+         cout << " " << GET_BIT(bitboard, square);
+      }
+      cout << endl;
+   }
+   cout << endl << "    a b c d e f g h" << endl;
+
+   cout << endl << "board:    " << bitboard << endl << endl;
+}
